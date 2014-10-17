6de4a34c74f7c16a08f66a7e9409637ee198b1cf
Justin Tulloss <justin.tulloss@gmail.com>
2014-10-17 16:03:05 -0700

    Clean up Reply methods
    - Remove the error from those methods that did return an error
    - Rename HttpError to HttpErrorReply

--- reply.go
+++ reply.go
@@ -15,18 +15,18 @@ func (s *Service) ErrorReply(err error, w http.ResponseWriter) {
 	debug.PrintStack()
 	if s.Env.InProd() {
 		// If we're in prod, we don't want to reveal internal server details
-		s.HttpError(w, "Internal Server Error", http.StatusInternalServerError)
+		s.HttpErrorReply(w, "Internal Server Error", http.StatusInternalServerError)
 	} else {
 		stack := make([]byte, 4096)
 		b := bytes.NewBuffer(stack)
 		bytesWritten := runtime.Stack(b.Bytes(), false)
 		b.Truncate(bytesWritten)
 		message := strings.Join([]string{err.Error(), b.String()}, "\n")
-		s.HttpError(w, message, http.StatusInternalServerError)
+		s.HttpErrorReply(w, message, http.StatusInternalServerError)
 	}
 }
 
-func (s *Service) HttpError(w http.ResponseWriter, message string, code int) {
+func (s *Service) HttpErrorReply(w http.ResponseWriter, message string, code int) {
 	reply := map[string]interface{}{
 		"status":  "error",
 		"code":    code,
@@ -42,25 +42,25 @@ func (s *Service) HttpError(w http.ResponseWriter, message string, code int) {
 }
 
 func (s *Service) NotFound(w http.ResponseWriter) {
-	s.HttpError(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+	s.HttpErrorReply(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 }
 
-func (s *Service) Reply(response interface{}, w http.ResponseWriter) error {
+func (s *Service) Reply(response interface{}, w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	jsonEncoder := json.NewEncoder(w)
 	if err := jsonEncoder.Encode(response); err != nil {
 		s.ErrorReply(err, w)
-		return err
+		return
 	}
-	return nil
+	return
 }
 
-func (s *Service) SuccessReply(metadata map[string]interface{}, w http.ResponseWriter) error {
+func (s *Service) SuccessReply(metadata map[string]interface{}, w http.ResponseWriter) {
 	rep := map[string]interface{}{"status": "OK"}
 	if metadata != nil {
 		for k, v := range metadata {
 			rep[k] = v
 		}
 	}
-	return s.Reply(rep, w)
+	s.Reply(rep, w)
 }
