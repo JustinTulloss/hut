3bb0c75b1b971ea06448fb6b4849c4e1e9b2c4ec
Justin Tulloss <justin.tulloss@gmail.com>
2015-02-13 15:08:27 -0800

    We have a prefix, remove the TODO

--- stats.go
+++ stats.go
@@ -12,7 +12,6 @@ func (s *Service) NewStatsd() (client Statter) {
 		client, _ = statsd.NewNoop()
 		return client
 	}
-	// TODO: put the right prefix in here
 	client, err := statsd.New(address, s.name)
 	if err != nil {
 		s.Log.Error().Printf("Could not connect to statsd: %s\n", err)
